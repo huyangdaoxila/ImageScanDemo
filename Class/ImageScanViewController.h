8c6040244760f32721e6819cab9ff86280eb424c
huyang <daoxila@iMac.local>
2015-08-21 11:48:44 +0800

    添加图片  , 并且引入Masonry第三方库

---
+++ Class/ImageScanViewController.h
@@ -0,0 +1,13 @@
+//
+//  ImageScanViewController.h
+//  ImageScanDemo
+//
+//  Created by huyang on 15/8/21.
+//  Copyright (c) 2015年 huyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImageScanViewController : UIPageViewController
+
+@end
---
+++ Class/SingleImageScanViewController.h
@@ -0,0 +1,13 @@
+//
+//  SingleImageScanViewController.h
+//  ImageScanDemo
+//
+//  Created by huyang on 15/8/21.
+//  Copyright (c) 2015年 huyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SingleImageScanViewController : UIViewController
+
+@end
